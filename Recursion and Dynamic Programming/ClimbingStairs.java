d573c1f5ad0690b1f6a21241f1a5be02c5fed508
Alex Oh <oalex@knights.ucf.edu>
2022-02-06 09:04:38 -0500

    Dynamic Programming Solution to LeetCode 70. Climbing Stairs

--- Recursion and Dynamic Programming/ClimbingStairs.java
+++ Recursion and Dynamic Programming/ClimbingStairs.java
@@ -7,7 +7,7 @@ public class ClimbingStairs {
         int [] memo = new int [n+1];
         return climbCount(n, 0, memo);
     }
-    //Memoization
+    //Memoization TOP DOWN
     public static int climbCount(int n, int count, int [] memo){
         if(n < 0) return 0;
         if(n == 0) return 1;
@@ -23,6 +23,21 @@ public class ClimbingStairs {
         if(n < 0) return 0;
         if(n == 0) return 1;
         return climbCount2(n-1, count+1) + climbCount2(n-2, count+1);
+    }
+
+    //BOTTOM UP DYNAMIC PROGRAMMING
+    public static int climbCount3(int n){
+        if(n == 1) return 1;
+        int [] dp = new int [n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+
     }
 
 }
