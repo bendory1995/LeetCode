1b6bc30e202c03407371a35ccb6ef0371883aefd
Alex Oh <oalex@knights.ucf.edu>
2022-02-08 09:33:06 -0500

    LeetCode 139. Word Break DP + Memoization Solution

--- Recursion and Dynamic Programming/WordBreak.java
+++ Recursion and Dynamic Programming/WordBreak.java
@@ -3,7 +3,8 @@ public class WordBreak {
     public static void main(String[] args){
 
     }
-    public static boolean wordBreakMemo(String s, List<String> wordDict){
+    //Bottom Up Solution
+    public static boolean wordBreakDP(String s, List<String> wordDict){
         int length = s.length();
         boolean[] dp = new boolean[length + 1];
         
@@ -17,5 +18,17 @@ public class WordBreak {
             }
         }
         return dp[length];
+    }
+    //Recursive Solution with Memoization
+    public boolean wordBreakMemo(String s, Set<String> wordDict, int start, Boolean [] memo){
+        if(start == s.length()) return true;
+        if(memo[start] != null) return memo[start];
+        
+        for(int end = start + 1; end < s.length() + 1; end++){
+            if(wordDict.contains(s.substring(start, end)) && wordBreakMemo(s, wordDict, end, memo)){
+                return memo[start] = true;
+            }
+        }
+        return memo[start] = false;
     }
 }
