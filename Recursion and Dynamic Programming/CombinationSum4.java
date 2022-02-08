526dc38190d86d8304ee168570b3f60ba77b8a73
Alex Oh <oalex@knights.ucf.edu>
2022-02-08 10:21:42 -0500

    LeetCode 377. Combination Sum IV Top-Down Solution

---
+++ Recursion and Dynamic Programming/CombinationSum4.java
@@ -0,0 +1,27 @@
+public class CombinationSum4 {
+    public static void main(String[] args){
+
+    }
+
+    public static int combinationSum4(int [] nums, int target){
+        return combinationMemo(nums, target, new Integer[target+1]);
+        
+    }
+
+    public static int combinationMemo(int [] nums, int target, Integer[] memo){
+        if (target == 0) return 1;
+        if (target < 0) return 0;
+        if (memo[target] != null){
+            return memo[target];
+        }
+
+        int total = 0;
+
+        for(int i = 0 ;i < nums.length; i++){
+            total = total + combinationMemo(nums, target - nums[i], memo);
+        }
+        memo[target] = total;
+
+        return memo[target];
+    }
+}
