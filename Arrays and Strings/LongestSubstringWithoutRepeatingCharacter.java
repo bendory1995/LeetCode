fd646a95b0e5c549d6fe639a052df861246124fe
Alex Oh <oalex@knights.ucf.edu>
2022-02-16 12:58:37 -0500

    LeetCode 3. Longest Substring Without Repeating Characters Sliding Window Solution

---
+++ Arrays and Strings/LongestSubstringWithoutRepeatingCharacter.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class LongestSubstringWithoutRepeatingCharacter {
+    public static void main(String[] args){
+
+    }
+    public int lengthOfLongestSubstring(String s){
+        Set<Character> hashset = new HashSet<>();
+
+        int slow = 0;
+        int fast = 0;
+        int max = 0;
+
+        while(fast < s.length()){
+            
+        }
+
+    }
+    
+}
