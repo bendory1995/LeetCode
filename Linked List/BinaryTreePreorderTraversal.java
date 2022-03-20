8ef1a2a81f19679a69c70eb5364e28def27061d1
Alex Oh <oalex@knights.ucf.edu>
2022-03-20 16:53:05 -0400

    Binary Tree Preorder Traversal Solution

---
+++ Linked List/BinaryTreePreorderTraversal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class BinaryTreePreorderTraversal {
+    List<Integer> list = new ArrayList<>();
+    public static void main(String[] args){
+
+    }
+
+    public static List<Integer> preorder(TreeNode root){
+        
+    }
+}
