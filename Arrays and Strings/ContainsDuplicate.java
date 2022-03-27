ccf7f37a600ef8559ce0dc8feae85e05c17403bd
Alex Oh <oalex@knights.ucf.edu>
2022-03-27 19:46:44 -0400

    ContainsDuplicate Review

--- Arrays and Strings/ContainsDuplicate.java
+++ Arrays and Strings/ContainsDuplicate.java
@@ -6,12 +6,11 @@ public class ContainsDuplicate {
 
     //HashMap O(n) Solution
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i = 0;i < nums.length; i++){
-            if(set.contains(nums[i])) return true;
-            else{
-                set.add(nums[i]);
-            }
+        Set<Integer> hs = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(hs.contains(nums[i])) return true;
+            hs.add(nums[i]);
+            
         }
         return false;
     }
