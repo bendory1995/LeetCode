fe51b1a2b07cd82eb7ac8369d231619c26243e86
Alex Oh <oalex@knights.ucf.edu>
2022-03-29 00:55:10 -0400

    Product of Array Except Self Review. O(n) Time and O(n) Space

--- Arrays and Strings/ProductOfArrayExceptSelf.java
+++ Arrays and Strings/ProductOfArrayExceptSelf.java
@@ -3,32 +3,22 @@ public class ProductOfArrayExceptSelf {
         
     }
     public int[] productExceptSelf(int[] nums) {
-        //answer array
-        int [] ans = new int[nums.length];
-        
-        //Left Array and Right Array
-        int [] left = new int [nums.length];
+        int [] left = new int[nums.length];
         int [] right = new int [nums.length];
-        
-        //Initialize the Array to 1 on ends
-        left[0] = 1;
-        right[right.length-1] = 1;
-        
-        //Left Array Calculation O(n)
-        for(int i = 1; i < left.length; i++){
-            left[i] = nums[i-1] * left[i-1]; 
-        }
-        //Right Array Calculation O(n)
-        for(int j = nums.length-2; j >= 0; j--){
-            right[j] = nums[j+1] * right[j+1];
 
+        left[0] = 1;
+        right[nums.length-1] = 1;
+        for(int i = 1; i < left.length; i++){
+            left[i] = left[i-1]* nums[i-1];
         }
-        
-        //Calculating Answer Array
+        for(int i = nums.length-2; i >= 0; i--){
+            right[i] = right[i+1]* nums[i+1];
+        }
+
+        int [] ans = new int[nums.length];
         for(int i = 0; i < nums.length; i++){
-            ans[i] = left[i] * right[i];
+            ans[i] = left[i]* right[i];
         }
-        
         return ans;
     }
 }
