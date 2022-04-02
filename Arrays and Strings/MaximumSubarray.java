d82fc4bc039af372aa9fa98c5e4f0e904b622d3f
Alex Oh <oalex@knights.ucf.edu>
2022-04-02 08:33:03 -0400

    LeetCode 53. Maximum Subarray Review. Brute force solution and Kadane's Algorithm.

--- Arrays and Strings/MaximumSubarray.java
+++ Arrays and Strings/MaximumSubarray.java
@@ -3,16 +3,26 @@ public class MaximumSubarray {
 
     }
     public int maxSubArray(int[] nums) {
-        int min = nums[0];
-        int totalMax = nums[0];
-        int localMax = nums[0];
-        
-        for(int i = 1; i < nums.length; i++){
-            if(localMax < 0) localMax = 0;
-            localMax = localMax + nums[i];
-            if(localMax > totalMax) totalMax = localMax;
+        if(nums.length == 1) return nums[0];
+        int currentMax = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            int sum  = nums[i];
+            currentMax = Math.max(currentMax, sum);
+            for(int j = i+1; j < nums.length; j++){
+                sum = sum + nums[j];
+                currentMax = Math.max(sum, currentMax);
+            }
         }
-        
+        return currentMax;
+    }
+    public int maxSubArray2(int [] nums){
+        int currentMax = nums[0];
+        int totalMax = Integer.MIN_VALUE;
+
+        for(int i = 1; i < nums.length; i++){
+            currentMax = Math.max(currentMax + nums[i], nums[i]);
+            totalMax =  Math.max(totalMax, currentMax);
+        }
         return totalMax;
     }
 }
